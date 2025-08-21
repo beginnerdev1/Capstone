b0dafcb1c17449cb8ed1ef9825b403ca388ebdfa
Your Quiel <caspequiel27@gmail.com>
2025-08-21 12:59:10 +0800

    index added to public

--- public/index.php
+++ public/index.php
@@ -48,7 +48,7 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 
 // LOAD OUR PATHS CONFIG FILE
 // This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . 'app/Config/Paths.php';
+require FCPATH . '../app/Config/Paths.php';
 $system_path = 'system';
 $application_folder = 'app';
 
