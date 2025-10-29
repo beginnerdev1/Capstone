9f67e79993cb84cf2986d37804944d6b4d1e8e2d
Your Quiel <caspequiel27@gmail.com>
2025-10-29 15:29:38 +0800

    Fixed navbar (admin)

--- app/Views/admin/layouts/navbar.php
+++ app/Views/admin/layouts/navbar.php
@@ -4,14 +4,15 @@
         <i class="bi bi-list"></i>
         </button>
         <a class="navbar-brand" href="#">Start Bootstrap</a>
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+        <!--<button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>-->
 
         <div class="d-flex align-items-center">
-        <a href="#" class="text-light me-3"><i class="bi bi-person-circle"></i></a>
+       
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li> <a href="#" class="dropdown-item"><i class="bi bi-person-circle"></i> Profile</a></li>
                         <li><a class="dropdown-item" href="#!">Settings</a></li>
                         <li><a class="dropdown-item" href="#!">Activity Log</a></li>
                         <li><hr class="dropdown-divider" /></li>
